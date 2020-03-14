1587efc8183d637ec3f9fede883dd797ae9c5984
Johannes Hufnagl <e11918516@student.tuwien.ac.at>
2020-03-14 17:40:27 +0100

    implemented the methods in Vector3

--- src/Vector3.java
+++ src/Vector3.java
@@ -23,45 +23,41 @@ public class Vector3 {
 
     // Returns the sum of this vector and vector 'v'.
     public Vector3 plus(Vector3 v) {
-
-        //TODO: implement method.
-        return null;
+        return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z);
     }
 
     // Returns the product of this vector and 'd'.
     public Vector3 times(double d) {
-
-        //TODO: implement method.
-        return null;
+        return new Vector3(this.x * d, this.y * d, this.z * d);
     }
 
     // Returns the sum of this vector and -1*v.
     public Vector3 minus(Vector3 v) {
-
-        //TODO: implement method.
-        return null;
+        return new Vector3(this.x - v.x, this.y - v.y, this.z - v.z);
     }
 
     // Returns the Euclidean distance of this vector
     // to the specified vector 'v'.
     public double distanceTo(Vector3 v) {
+        double dX = this.x - v.x;
+        double dY = this.y - v.y;
+        double dZ = this.z - v.z;
 
-        //TODO: implement method.
-        return -1d;
+        return Math.sqrt(dX * dX + dY * dY + dZ * dZ);
     }
 
     // Returns the length (norm) of this vector.
     public double length() {
-
-        //TODO: implement method.
-        return 0;
+        return distanceTo(this);
     }
 
     // Normalizes this vector: changes the length of this vector such that it becomes one.
     // The direction and orientation of the vector is not affected.
     public void normalize() {
-
-        //TODO: implement method.
+        double length = length();
+        this.x /= length;
+        this.y /= length;
+        this.z /= length;
     }
 
     // Draws a filled circle with the center at (x,y) coordinates of this vector
@@ -74,17 +70,14 @@ public class Vector3 {
     // Returns the coordinates of this vector in brackets as a string
     // in the form "[x,y,z]", e.g., "[1.48E11,0.0,0.0]".
     public String toString() {
-
-        //TODO: implement method.
-        return "";
+        return "[" + this.x + ',' + this.y + ',' + this.z + ']';
     }
 
     // Prints the coordinates of this vector in brackets to the console (without newline)
     // in the form [x,y,z], e.g.,
     // [1.48E11,0.0,0.0]
     public void print() {
-
-        //TODO: implement method.
+        System.out.println(toString());
     }
 
 }
